88d11b03fc31f551fb2ded62d8fd785bb2ddf746
Shantnu Aggarwal <97806563+shantnuagarwal@users.noreply.github.com>
2022-02-08 23:08:46 +0000

    Add files via upload

---
+++ healthcare/client/src/helpers/users.js
@@ -0,0 +1,9 @@
+export const doctors = [
+    { account: 1, name: 'Dr. Thompson' },
+    { account: 2, name: 'Dr. Alexander' }
+]
+
+export const patients = [
+    { account: 3, name: 'Mr. Harry' },
+    { account: 4, name: 'Mrs. Potter' }
+]
